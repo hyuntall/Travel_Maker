5121599d2a61ae29472d8699c4ed91079032df8a
hyuntall <hyunchul1998@naver.com>
2023-05-15 14:05:29 +0900

    create homeView and add axios and bootstrap

--- FrontEnd/enjoy-trip/src/views/HomeView.vue
+++ FrontEnd/enjoy-trip/src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home row">
+    <div class="col-5 align-self-center">
+      <h2>Enjoy Trip</h2>
+      <p>enjoy trip 프로젝트 설명</p>
+      <button class="rounded p-3 mb-2 bg-info text-dark border border-info">시작하기</button>
+    </div>
+    <img class="col-7" alt="Vue logo" src="../assets/main_page.jpg" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: {},
+};
 </script>
+
+<style scoped></style>
