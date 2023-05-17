a94fabf323c93977e9cf86b3ec7952d92e4f19ea
= <crumbled23@gmail.com>
2023-05-17 15:46:03 +0900

    cors

--- BackEnd/EnjoyTrip/src/main/java/com/ssafy/controller/QnaController.java
+++ BackEnd/EnjoyTrip/src/main/java/com/ssafy/controller/QnaController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.dto.QnaDto;
@@ -25,6 +27,8 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/qna")
+@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT,
+        RequestMethod.DELETE })
 public class QnaController {
 	
 	@Autowired
@@ -44,19 +48,22 @@ public class QnaController {
 	}
 	
 	@DeleteMapping("/delete/{idx}")
-	public void delete(@PathVariable int idx) throws SQLException {
+	public ResponseEntity<?> delete(@PathVariable int idx) throws SQLException {
 		qnasvc.delete(idx);
+		return new ResponseEntity<Void>(HttpStatus.OK);
 	}
 	
 	@PutMapping("/update")
-	public void update(@RequestBody QnaUserDto dto) throws SQLException {
+	public ResponseEntity<?> update(@RequestBody QnaUserDto dto) throws SQLException {
 		QnaDto qna = new QnaDto(0,dto.getTitle(),dto.getContent(),usvc.selectById(dto.getUser_id()).getIdx());
 		qnasvc.update(qna);
+		return new ResponseEntity<Void>(HttpStatus.OK);
 	}
 	
 	@PostMapping("/write")
-	public void write(@RequestBody QnaUserDto dto) throws SQLException{
+	public ResponseEntity<?> write(@RequestBody QnaUserDto dto) throws SQLException{
 		QnaDto qna = new QnaDto(0,dto.getTitle(),dto.getContent(),usvc.selectById(dto.getUser_id()).getIdx());
 		qnasvc.insert(qna);
+		return new ResponseEntity<Void>(HttpStatus.OK);
 	}
 }
