4d5c908e493f300b52a4632beb1c552ea955b008
= <crumbled23@gmail.com>
2023-05-19 11:05:07 +0900

    homeview btn

--- FrontEnd/enjoy-trip/src/views/HomeView.vue
+++ FrontEnd/enjoy-trip/src/views/HomeView.vue
@@ -3,7 +3,7 @@
     <div class="col-5 align-self-center main-text">
       <h2>Travel Maker</h2>
       <p>Travel Maker 프로젝트 설명</p>
-      <button class="rounded p-3 mb-2 bg-info text-dark border border-info">시작하기</button>
+      <button class="">시작하기</button>
     </div>
     <img class="col-7" alt="Vue logo" src="../assets/main_page.jpg" />
   </div>
@@ -25,4 +25,9 @@ export default {
 .main-text {
   text-align: center;
 }
+
+.main-text h2 {font-weight: 800px; font-size: 50px;}
+
+.main-text button {border: 2px solid #706565; padding: 8px; background-color: #fff; font-weight: 700; color:#706565;}
+.main-text button:hover {background-color: #706565; color:#fff}
 </style>
